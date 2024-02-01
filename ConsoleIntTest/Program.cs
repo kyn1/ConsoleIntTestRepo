b6fbb803cd2bb69d268f1be705dea8560c0ff62e
Kyn1 <setor.harris@yahoo.com>
2024-02-01 12:51:45 -0800

    Add project files.

---
+++ ConsoleIntTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleIntTest
+{
+    class Program
+    {
+        private const string dllPath = @"C:\Users\hfumato\source\repos\DllRepo\x64\Debug\DllRepo.dll";
+        [DllImport(dllPath)]
+
+        private static extern int Multiply(int a, int b);
+
+        static void Main(string[] args)
+        {
+            int num1 = 43;
+            int num2 = 4;
+
+            int result = Multiply(num1, num2);
+            Console.WriteLine($"Result from multiplication from cpp: {result}");
+        }
+    }
+}
