4510bcbd589a56d1e616d50e7981b4c2856e3841
Kyn1 <setor.harris@yahoo.com>
2024-02-09 10:51:49 -0800

    Added a new dll path and added a division method

--- ConsoleIntTest/Program.cs
+++ ConsoleIntTest/Program.cs
@@ -9,18 +9,24 @@ namespace ConsoleIntTest
 {
     class Program
     {
-        private const string dllPath = @"C:\Users\hfumato\source\repos\DllRepo\x64\Debug\DllRepo.dll";
+        private const string dllPath = @"C:\Users\hfumato\source\repos\CppDll2\x64\Debug\CppDll2.dll";
         [DllImport(dllPath)]
+        public static extern int Multiply(int a, int b);
 
-        private static extern int Multiply(int a, int b);
+        [DllImport(dllPath)]
+        public static extern double Divide(double a, double b);
 
         static void Main(string[] args)
         {
             int num1 = 43;
             int num2 = 4;
 
+            double num3 = 54.0;
+            double num4 = 2.0;
+
             int result = Multiply(num1, num2);
-            Console.WriteLine($"Result from multiplication from cpp: {result}");
+            double resultdivde = Divide(num3, num4);
+            Console.WriteLine($"Result from multiplication from cpp:{result} and result for division cpp: {resultdivde}" );
         }
     }
 }
